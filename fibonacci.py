46eb4bc470c894ea8b31da560392edddfba319d1
vedcoder <gupta.vidit@icloud.com>
2019-10-26 17:03:16 +0530

    c cahvcuashicjab8ysvcasca

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,13 @@
 
-y = 1
-oldy = 0
+num1 = 0
+num2 = 1
 numofFibon = int(input("please enter a number: "))
-
+print(f"{num1} + 0 = 0")
+print(f"{num1} + 1 = 1")
 for x in range(0, numofFibon-2):
 
-    y = oldy + y
-    oldy = y
-print(y)
+    newNum = num1 + num2
+    print(f"{num1} + {num2} = {newNum}")
+    num1 = num2
+    num2 = newNum
+
