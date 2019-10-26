9f21256543ac8fcb0c6964b7d96625941af1e1e9
vedcoder <gupta.vidit@icloud.com>
2019-10-26 17:10:55 +0530

    c cahvcuashicjab8ysvcasca

--- movies/inheritence.py
+++ movies/inheritence.py
@@ -1,26 +1,27 @@
 class Parent():
-    def __init__(self,last_name,eye_color):
+    def __init__(self, last_name, eye_color):
         print("parent constructer called")
-        self.last_name=last_name
-        self.eye_color=eye_color
+        self.last_name = last_name
+        self.eye_color = eye_color
+
     def show_info(self):
         print("Last name- "+self.last_name)
         print("Eye color- "+self.eye_color)
 
 
-
 class Child(Parent):
-    def __init__(self,last_name,eye_color,number_of_toys):
+    def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructer called")
-        Parent.__init__(self,last_name,eye_color)
-        self.number_of_toys=number_of_toys
+        Parent.__init__(self, last_name,eye_color)
+        self.number_of_toys = number_of_toys
+
     def show_info(self):
         print("Last name- "+self.last_name)
         print("Eye color- "+self.eye_color)
         print("Number of toys- "+str(self.number_of_toys))
 
 
-billy_cyrus=Parent("cyrus","blue")
+billy_cyrus = Parent("cyrus","blue")
 #billy_cyrus.show_info():
-milly_cyrus=Child("cyrus","blue",5)
+milly_cyrus = Child("cyrus","blue",5)
 milly_cyrus.show_info()
