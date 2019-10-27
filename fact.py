1cf44da8912e00509ed281d4d385c3efaacb8fd6
vedcoder <gupta.vidit@icloud.com>
2019-10-27 09:56:33 +0530

    c cahvcuashicjab8ysvcasca

--- fact.py
+++ fact.py
@@ -1,8 +1,13 @@
 user_input = int(input("enter a number: "))
 
+
 def fact2(y):
+
     if y>1:
-        return (y*fact2(y-1))
-    return (1)
+
+        return y*fact2(y-1)
+
+    return 1
+
 
 print(fact2(user_input))
